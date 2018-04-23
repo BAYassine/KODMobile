12f8c2adeea3d57ed23aca398f83afb77f98f1ed
BAYassine <lomyassen@gmail.com>
2018-04-24 00:48:15 +0100

    Core.App

--- src/Core/App.java
+++ src/Core/App.java
@@ -57,39 +57,7 @@ public class App {
             current.show();
             return;
         }
-        Form hi = new Form("Welcome", new BorderLayout(BorderLayout.CENTER_BEHAVIOR_CENTER_ABSOLUTE));
-        final Label apple = new Label(theme.getImage("apple-icon.png")); 
-        final Label android = new Label(theme.getImage("android-icon.png")); 
-        final Label windows = new Label(theme.getImage("windows-icon.png")); 
-        Button getStarted = new Button("Let's Get Started!");
-        FontImage.setMaterialIcon(getStarted, FontImage.MATERIAL_LINK);
-        getStarted.setUIID("GetStarted");
-        hi.addComponent(BorderLayout.CENTER, 
-                LayeredLayout.encloseIn(
-                        BoxLayout.encloseY(
-                                new Label(theme.getImage("duke-no-logos.png")),
-                                getStarted
-                        ),
-                        FlowLayout.encloseRightMiddle(apple)
-                    )
-        );
-        
-        getStarted.addActionListener((e) -> {
-            execute("https://www.codenameone.com/developers.html");
-        });
-        
-        new UITimer(() -> {
-            if(apple.getParent() != null) {
-                apple.getParent().replace(apple, android, CommonTransitions.createFade(500));
-            } else {
-                if(android.getParent() != null) {
-                    android.getParent().replace(android, windows, CommonTransitions.createFade(500));
-                } else {
-                    windows.getParent().replace(windows, apple, CommonTransitions.createFade(500));
-                }                
-            }
-        }).schedule(2200, true, hi);
-        hi.show();
+        new HomeController().init();
     }
 
     public void stop() {
